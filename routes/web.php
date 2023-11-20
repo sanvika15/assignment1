6c52c1e784d88160ed932e524a36bd5704a09164
sanvika15 <137372400+sanvika15@users.noreply.github.com>
2023-11-20 21:21:19 +0530

    Add Routes and Empty views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view("/", 'home')->name('home');
+Route::view("/about", 'about')->name('about');
+Route::view("/history", 'history')->name('history');
+Route::view("/awards", 'awards')->name('awards');
+Route::view("/contact", 'contact')->name('contact');
+
